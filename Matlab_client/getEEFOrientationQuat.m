e678937da185bab957ccbc817423db601e1c7a69
Modi1987 <ms@uc.pt>
2017-08-07 18:33:19 +0100

    Documentation of the function (getEEFOrientationQuat.p)

---
+++ Matlab_client/getEEFOrientationQuat.m
@@ -0,0 +1,17 @@
+%% This function is used to get the orientation of the 
+% media flange of the KUKA iiwa 7 R 800 robot.
+
+%% Syntax:
+% [q]= getEEFOrientationQuat(t)
+
+%% About:
+% This function is used to get the orientation of the 
+% media flange of the KUKA iiwa 7 R 800 robot. The orientation is described
+% by a quaternion
+
+%% Arreguments:
+% q: is the quaternion descriping the orientation of the end-effector of
+% the media flange of the robot.
+% t : is the TCP/IP comunication object.
+
+%% Copy right, Mohammad SAFEEA 7th of August 2017.
