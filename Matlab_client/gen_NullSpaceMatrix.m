b8ed36a57d0408697979dd18ff4653a906d59121
Modi1987 <ms@uc.pt>
2018-04-18 17:35:59 +0100

    For calculating null space matrix of the robot

---
+++ Matlab_client/gen_NullSpaceMatrix.m
@@ -0,0 +1,22 @@
+function [ N ] = gen_NullSpaceMatrix(q)
+ % About
+ %-----------
+ % This function is used for calculating the Null space projection matrix
+ % of the KUKA iiwa 7 R 800 manipulator 
+ 
+% Arreguments:
+%--------------------
+% q: is 1x7 vector, joint nagles vector of the manipulator
+
+% Return value:
+%--------------------
+% N: 7x7 matrix, null space projection matrix for the KUKA iiwa 7 R 800.
+
+% Copyright: Mohammad SAFEEA, 17th-April-2018
+
+TefTool=eye(4);
+[eef_transform,J]=gen_DirectKinematics(q,TefTool);
+N=eye(7)-J'*inv(J*J')*J';
+
+end
+
