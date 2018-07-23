e83d9f3f7628801663f0077daa86077a780d6252
Modi1987 <ms@uc.pt>
2018-07-23 12:40:47 +0100

    Solver updated

--- Matlab_client/kukaDLSSolver_1.m
+++ Matlab_client/kukaDLSSolver_1.m
@@ -1,5 +1,4 @@
-function [ qs ] = kukaDLSSolver_1( qin, Tt, TefTool,n,lambda,dh )
-%% This is a DLS sovler of the inverse kinematics for the KUKA iiwa 7 R 800 robot.
+%% A DLS sovler of the inverse kinematics for the KUKA iiwa 7 R 800 robot.
 
 % Inputs:
 %-------------
@@ -14,58 +13,5 @@ function [ qs ] = kukaDLSSolver_1( qin, Tt, TefTool,n,lambda,dh )
 % Outputs:
 %-------------
 % qs: the solution joint angles of the robot.
-
-q=qin;
-for i=1:n
-    xt=Tt(1:3,4);
-    [T,J]=directKinematics_1(q,TefTool,dh);
-    x=T(1:3,4);
-    % Linear displacment error
-    ex=xt-x; % error in positioning
-    % Orientation displacment errors
-    da=deltAngle(Tt(1:3,1:3),T(1:3,1:3));
-    eo=da; % error in orientation
-    e=[ex;eo];  % error vector
-    
-    % update the joint angles
-    JJT=J*J'+lambda*eye(6);
-    tempVec=JJT\e;
-    q=q+J'*tempVec;
-    
-end % loop n iterations
-qs=q;
-
-end
-
-
-
-function da=deltAngle(Rt,Rin)
-% This function is used to calculate the angular displacments from the
-% rotational matrix,
-
-% Inputs:
-% Rt:   The target rotational matrix.
-% Rin:  The initial rotational matrix.
-
-% Outputs:
-% da: angular displacments around the X,Y and Z axes.
-
-    deltaR=Rt-Rin;
-    R=Rin;
-    vec=[deltaR(:,1);
-        deltaR(:,2);
-        deltaR(:,3)];
-    M=[skew(R(1:3,1));
-        skew(R(1:3,2));
-        skew(R(1:3,3))];
-    da=-M\vec;
-end
-
-function X=skew(v)
-a=v(1);
-b=v(2);
-c=v(3);
-X=[0   -c   b ;
- c    0  -a ;
--b    a   0  ];
-end
+ 
+% Copyright: Mohamad SAFEEA, 23 - July - 2018
