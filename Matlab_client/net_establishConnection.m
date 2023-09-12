417242688cec9c496085533d18323795a3a32043
Mohammad SAFEEA <ms@uc.pt>
2023-09-12 19:47:58 +0200

    Adding support to Matlab 2023 in the net_establishConnection.m

--- Matlab_client/net_establishConnection.m
+++ Matlab_client/net_establishConnection.m
@@ -21,6 +21,7 @@ if ~exist('ip','var') || isempty(ip)
   warning('Connection could not be establised');
   return;
 else
+% try to connect using tcpip from instrumentation toolbox
     try
             port=30001;
             % Connect to server
@@ -29,10 +30,20 @@ else
             t.Timeout=3.0;
             fopen(t);
             pause(1);
+            return;
     catch exception
-       warning('Connection could not be establised');
+       warning('Connection could not be establised using tcpip from instrumentation toolbox');
     end
-
+% try to connect using tcpip supported by Matlab 2023 and above
+   try
+           port=30001;
+            % Connect to server
+            t= tcpclient(ip, port, "EnableTransferDelay",false, "ConnectTimeout", 3);
+            pause(1);
+            return;
+    catch exception
+       warning('Connection could not be establised using tcpclient supported by Matlab 2023 and above');
+    end
 end
 
 end
